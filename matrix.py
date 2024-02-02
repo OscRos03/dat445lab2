005b4fa707fc42da23e44e082b82ab05d0589fae
OscRos03 <oscros03@gmail.com>
2024-02-02 16:53:43 +0100

    kebab time

--- matrix.py
+++ matrix.py
@@ -1,14 +1,18 @@
-def transpose():
-    return
+def transpose(M):
+    Mt = []
+    if len(M) > 0:
+        for i in range(len(M[0])):
+            Mt.append([])
 
-def powers():
-    return
+    for column in range(len(M)):
+        for row in range(len(M[column])):
+            Mt[row].append(M[column][row])
+    return Mt
 
-def matmul():
-    return
-
-def invert():
-    return
-
-def loadtxt():
-    return
+def powers(M, n1, n2):
+    Mp = []
+    for i in range(len(M)):
+        Mp.append([])
+        for j in range(n1, n2+1):
+            Mp[i].append(M[i]**j)
+    return Mp
