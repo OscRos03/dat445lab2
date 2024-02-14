813024f3461be7ed10a6c25994c611ed5a0aacdf
OscRos03 <oscros03@gmail.com>
2024-02-14 17:11:28 +0100

    unfucked the code part 2

--- native_regression.py
+++ native_regression.py
@@ -5,34 +5,22 @@ import matplotlib.pyplot as plt
 Min_temp=10
 Max_temp=30
 Data=loadtxt(sys.argv[1])
-#Data=loadtxt("chirps.txt")
 
 Transposed_data=transpose(Data)
-
-
-
 
 X=Transposed_data[0]
 Y=Transposed_data[1]
 
-X_float=[float(x) for x in X]
-Y_float=[float(x) for x in Y]
-
-Xp  = powers(X_float,0,1)
-Yp  = powers(Y_float,1,1)
+Xp  = powers(X,0,1)
+Yp  = powers(Y,1,1)
 Xptransposed = transpose(Xp)
 
 [[b],[m]] = matmul(invert(matmul(Xptransposed,Xp)),matmul(Xptransposed,Yp))
 
-Y2=[b+m*x for x in X_float]
-
-
-
-
+Y2=[b+m*x for x in X]
 
 for x in range(Min_temp,Max_temp+1):
-    plt.plot(X_float,Y_float,'ro')
-    plt.plot(X_float,Y2)
-
+    plt.plot(X,Y,'ro')
+    plt.plot(X,Y2)
 
 plt.show()
